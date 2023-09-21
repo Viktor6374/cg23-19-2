617eea8c9644693cbf0e1958f3455ec8c374c9a0
Viktor Subbotin <zxcvbnmh123456ghh@mail.ru>
2023-09-21 23:14:06 +0300

    :))

--- src/infrastructure/loaders/p6imageloader.cpp
+++ src/infrastructure/loaders/p6imageloader.cpp
@@ -4,7 +4,25 @@ P6ImageLoader::P6ImageLoader()
 {
 }
 
-Image *P6ImageLoader::load(const std::ifstream &in)
+Image *P6ImageLoader::load(std::ifstream &in)
 {
-    return nullptr;
+    int w;
+    int h;
+    in >> w >> h;
+    int chislo;
+    in >> chislo;
+    std::vector<Pixel> vec;
+    char red;
+    char green;
+    char blue;
+    while (in.read(&red, 1)){
+        in.read(&green, 1);
+        in.read(&blue, 1);
+        Pixel point(red, green, blue);
+        vec.push_back(point);
+    }
+    if (vec.size() != w * h){
+        throw std::exception();
+    }
+    return new Image(w, h, vec);
 }
