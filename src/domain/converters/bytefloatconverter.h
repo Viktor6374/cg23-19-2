eff5d6eccbb72175061808907516eb9c3d916e01
NLM <nl_marianne@mail.ru>
2023-10-12 17:24:26 +0300

    feat: byte-float converter

---
+++ src/domain/converters/bytefloatconverter.h
@@ -0,0 +1,17 @@
+#ifndef BYTEFLOATCONVERTER_H
+#define BYTEFLOATCONVERTER_H
+
+#include "../entities/image.h"
+#include "../entities/bytepixel.h"
+#include "../entities/floatpixel.h"
+
+
+class ByteFloatConverter
+{
+public:
+    ByteFloatConverter();
+
+
+};
+
+#endif // BYTEFLOATCONVERTER_H
