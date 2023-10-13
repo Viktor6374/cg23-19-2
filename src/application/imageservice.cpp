1c4ab16ce83fec28b65ebe1d3f1249c5dc6b67d0
ZISRF <zeleninivans@gmail.com>
2023-10-13 15:11:16 +0300

    feat: update image service

--- src/application/imageservice.cpp
+++ src/application/imageservice.cpp
@@ -3,30 +3,41 @@
 ImageService::ImageService()
 {
     _image_repository = new ImageRepository();
+    _color_space_converter = new ColorSpaceConverter();
 
-    _base_image = nullptr;
+    _current_image = nullptr;
+
+    _current_color_space = RGB;
 }
 
 ImageService::~ImageService()
 {
-    delete _base_image;
+    delete _current_image;
 
     delete _image_repository;
+    delete _color_space_converter;
 }
 
 Image *ImageService::base_image()
 {
-    return _base_image;
+    return _current_image;
 }
 
 void ImageService::load_image(std::string file_path)
 {
-    delete _base_image;
+    delete _current_image;
 
-    _base_image = _image_repository->load(file_path);
+    _current_image = _image_repository->load(file_path);
 }
 
 void ImageService::save_image(std::string file_path, std::string image_type)
 {
-    _image_repository->save(_base_image, file_path, image_type);
+    _image_repository->save(_current_image, file_path, image_type);
+}
+
+void ImageService::convert_image_to_color_space(std::string color_space)
+{
+    ColorSpace new_color_space;
+
+    _color_space_converter->convert(_current_image, _current_color_space, new_color_space);
 }
--- src/application/imageservice.h
+++ src/application/imageservice.h
@@ -3,6 +3,8 @@
 
 #include "../domain/entities/image.h"
 #include "../infrastructure/imagerepository.h"
+#include "../domain/converters/colorspaceconverter.h"
+#include "../domain/models/colorspace.h"
 #include <string>
 
 class ImageService
@@ -16,10 +18,14 @@ public:
     void load_image(std::string file_path);
     void save_image(std::string file_path, std::string image_type);
 
+    void convert_image_to_color_space(std::string color_space);
+
 private:
     ImageRepository *_image_repository;
+    ColorSpaceConverter *_color_space_converter;
 
-    Image *_base_image;
+    Image *_current_image;
+    ColorSpace _current_color_space;
 };
 
 #endif // IMAGESERVICE_H
