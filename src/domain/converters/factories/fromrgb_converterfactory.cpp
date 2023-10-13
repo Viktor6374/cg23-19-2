b18d2ae849429a35d90e889fe6392c9bffdb7b7d
ZISRF <zeleninivans@gmail.com>
2023-10-13 14:56:15 +0300

    feat: update factories

--- src/domain/converters/factories/fromrgb_converterfactory.cpp
+++ src/domain/converters/factories/fromrgb_converterfactory.cpp
@@ -1,5 +1,11 @@
 #include "fromrgb_converterfactory.h"
 #include "../from_rgb/torgb_fromrgb_converter.h"
+#include "../from_rgb/tohsl_fromrgb_converter.h"
+#include "../from_rgb/tohsv_fromrgb_converter.h"
+#include "../from_rgb/toycbcr609_fromrgb_converter.h"
+#include "../from_rgb/toycbcr709_fromrgb_converter.h"
+#include "../from_rgb/toycocg_fromrgb_converter.h"
+#include "../from_rgb/tocmy_fromrgb_converter.h"
 #include <stdexcept>
 
 FromRGB_ConverterFactory::FromRGB_ConverterFactory()
@@ -8,12 +14,15 @@ FromRGB_ConverterFactory::FromRGB_ConverterFactory()
 
 FromRGB_Converter *FromRGB_ConverterFactory::create(ColorSpace to)
 {
-    if (to == RGB)
+    switch (to)
     {
-        return new ToRGB_FromRGB_Converter();
-    }
-    else
-    {
-        throw std::logic_error("Unsupported conversion");
+    case RGB : return new ToRGB_FromRGB_Converter();
+    case HSL : return new ToHSL_FromRGB_Converter();
+    case HSV : return new ToHSV_FromRGB_Converter();
+    case YCbCr609 : return new ToYCbCr609_FromRGB_Converter();
+    case YCbCr709 : return new ToYCbCr709_FromRGB_Converter();
+    case YCoCg : return new ToYCoCg_FromRGB_Converter();
+    case CMY : return new ToCMY_FromRGB_Converter();
+    default : throw std::logic_error("Unsupported conversion");
     }
 }
--- src/domain/converters/factories/torgb_converterfactory.cpp
+++ src/domain/converters/factories/torgb_converterfactory.cpp
@@ -1,19 +1,28 @@
 #include "torgb_converterfactory.h"
 #include "../to_rgb/fromrgb_torgb_converter.h"
+#include "../to_rgb/fromhsl_torgb_converter.h"
+#include "../to_rgb/fromhsv_torgb_converter.h"
+#include "../to_rgb/fromycbcr609_torgb_converter.h"
+#include "../to_rgb/fromycbcr709_torgb_converter.h"
+#include "../to_rgb/fromycocg_torgb_converter.h"
+#include "../to_rgb/fromcmy_torgb_converter.h"
 #include <stdexcept>
 
 ToRGB_ConverterFactory::ToRGB_ConverterFactory()
 {
 }
 
-ToRGB_Converter *ToRGB_ConverterFactory::create(ColorSpace to)
+ToRGB_Converter *ToRGB_ConverterFactory::create(ColorSpace from)
 {
-    if (to == RGB)
+    switch (from)
     {
-        return new FromRGB_ToRGB_Converter();
-    }
-    else
-    {
-        throw std::logic_error("Unsupported conversion");
+    case RGB : return new FromRGB_ToRGB_Converter();
+    case HSL : return new FromHSL_ToRGB_Converter();
+    case HSV : return new FromHSV_ToRGB_Converter();
+    case YCbCr609 : return new FromYCbCr609_ToRGB_Converter();
+    case YCbCr709 : return new FromYCbCr709_ToRGB_Converter();
+    case YCoCg : return new FromYCoCg_ToRGB_Converter();
+    case CMY : return new FromCMY_ToRGB_Converter();
+    default : throw std::logic_error("Unsupported conversion");
     }
 }
