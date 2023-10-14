8ffca442c8e63038ca969306689905084b4d992e
Viktor Subbotin <zxcvbnmh123456ghh@mail.ru>
2023-10-15 01:06:42 +0300

    fix: syntax

--- src/domain/converters/from_rgb/toycbcr709_fromrgb_converter.cpp
+++ src/domain/converters/from_rgb/toycbcr709_fromrgb_converter.cpp
@@ -4,19 +4,19 @@ ToYCbCr709_FromRGB_Converter::ToYCbCr709_FromRGB_Converter()
 {
 }
 
-void ToYCbCr709_FromRGB_Converter::convert(Image *image)
-{
-    for (auto pixel : image->pixels()) {
-        ToYCbCr709_FromRGB_Pixel_Converter(pixel.channels);
-    }
-}
-
 void ToYCbCr709_FromRGB_Pixel_Converter(float* channels) {
     float R = channels[0];
     float G = channels[1];
     float B = channels[2];
 
-    channels[0] = R * 0,2136 + G * 0,7152 + B * 0,0722;
-    channels[1] = R * -0,114572 - G * 0,385428 + B * 0,5 + 0.5;
-    channels[2] = R * 0.5 - G * 0,454153 - B * 0,045847 + 0.5;
+    channels[0] = R * 0.2136 + G * 0.7152 + B * 0.0722;
+    channels[1] = R * -0.114572 - G * 0.385428 + B * 0.5 + 0.5;
+    channels[2] = R * 0.5 - G * 0.454153 - B * 0.045847 + 0.5;
+}
+
+void ToYCbCr709_FromRGB_Converter::convert(Image *image)
+{
+    for (auto pixel : image->pixels()) {
+        ToYCbCr709_FromRGB_Pixel_Converter(pixel.channels);
+    }
 }
--- src/domain/converters/to_rgb/fromycbcr709_torgb_converter.cpp
+++ src/domain/converters/to_rgb/fromycbcr709_torgb_converter.cpp
@@ -4,19 +4,19 @@ FromYCbCr709_ToRGB_Converter::FromYCbCr709_ToRGB_Converter()
 {
 }
 
-void FromYCbCr709_ToRGB_Converter::convert(Image *image)
-{
-    for (auto pixel : image->pixels()) {
-        FromYCbCr709_ToRGB_Pixel_Converter(pixel.channels);
-    }
-}
-
 void FromYCbCr709_ToRGB_Pixel_Converter(float* channels) {
     float Y = channels[0];
     float Pb = channels[1] - 0.5;
     float Pr = channels[2] - 0.5;
 
-    channels[0] = Y + Pr * 1,5748;
-    channels[1] = Y - Pb * 0,187324 - Pr * 0,468124;
-    channels[2] = Y + Pb * 1,8556;
+    channels[0] = Y + Pr * 1.5748;
+    channels[1] = Y - Pb * 0.187324 - Pr * 0.468124;
+    channels[2] = Y + Pb * 1.8556;
+}
+
+void FromYCbCr709_ToRGB_Converter::convert(Image *image)
+{
+    for (auto pixel : image->pixels()) {
+        FromYCbCr709_ToRGB_Pixel_Converter(pixel.channels);
+    }
 }
