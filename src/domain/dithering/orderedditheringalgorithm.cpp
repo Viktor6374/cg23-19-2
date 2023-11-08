5e45dcfb745b5b011beb641b3ab3b07e9370516b
ZISRF <zeleninivans@gmail.com>
2023-11-08 23:28:58 +0300

    feat: add ordered dithering

--- src/domain/dithering/orderedditheringalgorithm.cpp
+++ src/domain/dithering/orderedditheringalgorithm.cpp
@@ -1,7 +1,27 @@
 #include "orderedditheringalgorithm.h"
+#include <cmath>
 
 OrderedDitheringAlgorithm::OrderedDitheringAlgorithm()
 {
+}
+
+const int M[8][8] = { { 0, 32, 8, 40, 2, 34, 10, 42 },
+                      { 48, 16, 56, 24, 50, 18, 58, 26 },
+                      { 12, 44, 4, 36, 14, 46, 6, 38 },
+                      { 60, 28, 52, 20, 62, 30, 54, 22 },
+                      { 3, 35, 11, 43, 1, 33, 9, 41 },
+                      { 51, 19, 59, 27, 49, 17, 57, 25 },
+                      { 15, 47, 7, 39, 13, 45, 5, 37 },
+                      { 63, 31, 55, 23, 61, 29, 53, 21 } };
+
+float get_m(int i, int w)
+{
+    int x = i % w;
+    int y = i / w;
+    x %= 8;
+    y %= 8;
+
+    return M[x][y] / 64.0;
 }
 
 unsigned char *OrderedDitheringAlgorithm::execute(Image *image, int bytes_count)
@@ -14,9 +34,16 @@ unsigned char *OrderedDitheringAlgorithm::execute(Image *image, int bytes_count)
     {
         for (int j = 0; j < 3; ++j)
         {
-            int channel = (unsigned char)(image->pixels()[i].channels[j] * byte_size);
+            float value = image->pixels()[i].channels[j];
+            value += (get_m(3 * i + j, 3 * image->width()) - 0.5) / bytes_count;
+            value = fminf(1, value);
+            value = fmaxf(0, value);
 
-            data[3 * i + j] = (unsigned char)(channel * 256 / byte_size);
+            int channel = (unsigned char)round(value * (byte_size - 1));
+            channel *= 255;
+            channel /= byte_size - 1;
+
+            data[3 * i + j] = channel;
         }
     }
 
