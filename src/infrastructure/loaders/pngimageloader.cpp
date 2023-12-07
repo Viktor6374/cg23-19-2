76392891db9f091b6062980018e95ec69b388598
Viktor Subbotin <zxcvbnmh123456ghh@mail.ru>
2023-12-07 11:31:25 +0300

    Update pngimageloader.cpp
    add download png

--- src/infrastructure/loaders/pngimageloader.cpp
+++ src/infrastructure/loaders/pngimageloader.cpp
@@ -174,16 +174,86 @@ QByteArray Compress(QByteArray postBody)
     return outBuf;
 }
 
+unsigned char Paeth_predictor(int left, int up, int upper_left){
+    unsigned char p = left + up - upper_left;
+    unsigned char p_left = abs(p - left);
+    unsigned char p_up = abs(p - up);
+    unsigned char p_upper_left = abs(p - upper_left);
+    if (p_left < p_up && p_left < p_upper_left){
+        return left;
+    } else if (p_up < p_left && p_up < p_upper_left){
+        return up;
+    } else {
+        return upper_left;
+    }
+}
 
-QByteArray removeEvery1280thByte(const QByteArray &original) {
+QByteArray filter(const QByteArray &original) {
     QByteArray result;
     result.reserve(original.size() - original.size() / (1280 * 3)); // Optional, to optimize memory allocation
+    std::vector<unsigned char> filter_bytes;
 
-    for (int i = 0; i < original.size(); ++i) {
-        if ((i + 1) % (1280 * 3) != 0) {
-            // Keep the byte if its position is not a multiple of 1280
-            result.append(original[i]);
+    int number_string = 0;
+    for (int i = 0; i < original.size(); i += 1280 * 3 + 1) {
+        unsigned char filter = original[i];
+        filter_bytes.push_back(filter);
+        switch (filter) {
+        case 0:
+            for (int j = i + 1; j < i + 1280 * 3 + 1; j++){
+                result.append(original[j]);
+            }
+            break;
+        case 1:
+            for (int j = i + 1; j < i + 1280 * 3 + 1; j++){
+                if (result.size() - 3 < number_string * 1280 * 3){
+                    result.append(original[j]);
+                    continue;
+                }
+                result.append(original[j] + result[result.size() - 3]);
+            }
+            break;
+        case 2:
+            for (int j = i + 1; j < i + 1280 * 3 + 1; j++){
+                if (result.size() - 1280 * 3 < 0){
+                    result.append(original[j]);
+                    continue;
+                }
+                result.append(original[j] + result[result.size() - 1280 * 3]);
+            }
+            break;
+        case 3:
+            for (int j = i + 1; j < i + 1280 * 3 + 1; j++){
+                unsigned char first = 0;
+                unsigned char second = 0;
+                if (result.size() - 3 >= number_string * 1280 * 3){
+                    first = result[result.size() - 3];
+                }
+                if (result.size() - 1280 * 3 >= 0){
+                    second = result[result.size() - 1280 * 3];
+                }
+                result.append(original[j] + (first + second) / 2);
+            }
+            break;
+        case 4:
+            for (int j = i + 1; j < i + 1280 * 3 + 1; j++){
+                unsigned char left = 0;
+                if (result.size() - 3 >= i + 1){
+                    left = result[result.size() - 3];
+                }
+                unsigned char up = 0;
+                if (result.size() - 1280 * 3 >= 0){
+                    up = result[result.size() - 1280 * 3];
+                }
+                unsigned char upper_left = 0;
+                if (result.size() - 1280 * 3 - 1 >= std::max(i - 1280 * 3 + 1, 0)){
+                    upper_left = result[result.size() - 1280 * 3 - 1];
+                }
+                result.append(original[j] + Paeth_predictor(left, up, upper_left));
+            }
+            break;
         }
+
+        number_string++;
     }
 
     return result;
@@ -238,7 +308,7 @@ Image *PNGImageLoader::load(std::ifstream &in) {
 
     QByteArray img(reinterpret_cast<const char*>(compressed_bytes.data()), static_cast<int>(compressed_bytes.size()));
     QByteArray uncompressed_bytes = qGzipUncompress(img);
-    uncompressed_bytes = removeEvery1280thByte(uncompressed_bytes);
+    uncompressed_bytes = filter(uncompressed_bytes);
 
     std::vector<Pixel> pixels;
     for (int i = 0; i < uncompressed_bytes.size() - 2; i += 3) {
